52bd9965ad62eba1e092df7e0bd6a88e24b97ee0
shibumi2906 <164049385+shibumi2906@users.noreply.github.com>
2024-06-03 13:45:04 +0300

    2 задача

---
+++ grafik.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Генерация двух наборов случайных данных
+num_samples = 100  # Количество образцов
+data1 = np.random.rand(num_samples)  # Первый набор данных
+data2 = np.random.rand(num_samples)  # Второй набор данных
+
+# Построение диаграммы рассеяния
+plt.figure(figsize=(10, 6))
+plt.scatter(data1, data2, alpha=0.5)
+plt.title('Scatter Plot of Two Random Data Sets')
+plt.xlabel('Data Set 1')
+plt.ylabel('Data Set 2')
+plt.grid(True)
+plt.show()
+
+# Эта строка помогает убедиться, что график отображается, особенно в средах, которые могут не отображать графики автоматически
+input("Press [enter] to finish.")
+
